1fc29ad2c3d7540ae9f1aff677584ab9ea91df90
Rea23 <36733245+Rea23@users.noreply.github.com>
2018-09-24 19:06:37 +0200

    Add files via upload

---
+++ ProvjeraIspravnogPodnaslovaTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using zavrsni_rad;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zavrsni_rad.Tests
+{
+    [TestClass()]
+    public class ProvjeraIspravnogPodnaslovaTests
+    {
+        [TestMethod()]
+        public void IzborPodnaslova_NaslovStruktPod_PodnaslovLabVjezbe()
+        {
+            //Priprema okruženja za testiranje
+            ProvjeraIspravnogPodnaslova stranica = new ProvjeraIspravnogPodnaslova();
+            //Izvođenje funkcionalnosti koja se testira
+            string podnaslov = stranica.IzborPodnaslova("Strukture podataka");
+            //Provjera ispravnosti
+            Assert.AreEqual("Laboratorijske vježbe", podnaslov);
+        }
+
+        [TestMethod()]
+        public void IzborPodnaslova_NaslovProgInz_PodnaslovProjekti()
+        {
+            //Priprema okruženja za testiranje
+            ProvjeraIspravnogPodnaslova stranica = new ProvjeraIspravnogPodnaslova();
+            //Izvođenje funkcionalnosti koja se testira
+            string podnaslov = stranica.IzborPodnaslova("Programsko inženjerstvo");
+            //Provjera ispravnosti
+            Assert.AreEqual("Projekti", podnaslov);
+        }
+    }
+}
---
+++ UnosUBazuTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using zavrsni_rad;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zavrsni_rad.Tests
+{
+    [TestClass()]
+    public class UnosUBazuTests
+    {
+        [TestMethod()]
+        public void UnosUBazu_UspjesanUnos_VracenTrue()
+        {
+            //Priprema okruženja za testiranje
+            stub pisanje = new stub();
+            UnosUBazu izvrsavanje = new UnosUBazu();
+            //Izvođenje funkcionalnosti koja se testira
+            string vraceno = izvrsavanje.Spremi("11-120", "Ante", "Antic", pisanje);
+            string id = "11-120";
+            string ime = "Ante";
+            string prezime = "Antic";
+            //Provjera ispravnosti
+            Assert.IsTrue(vraceno == "INSERT INTO[Student](ID_studenta ,Ime_studenta, Prezime_studenta)VALUES(@id, @ime, @prezime)");
+        }
+    }
+}
